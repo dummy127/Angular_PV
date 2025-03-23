699888a1390e0ad84b8b730e594b035267e3fe9d
user.email <huzaifaansari9270@gmail.com>
2025-03-23 08:47:02 +0530

    23-03-2025

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AddressComponent } from './address/address.component';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutComponent,AddressComponent,ProductComponent],//imports for routing purpose
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myapp';
+  name:string = 'Aradhya';
+  lastname:string = "ambole";
+  pincode:number = 400025;
+  
 }
---
+++ readme_file.txt
@@ -0,0 +1,11 @@
+when you pull this project from github don't forget to run npm install on your system
+
+npm install -g typescript 
+
+ng new application_name
+
+ng serve to run app in localhost in cmd prompt
+
+ng generate component your_component_name //to create a new component
+
+short cut for the above command is :ng g c your_component_name
